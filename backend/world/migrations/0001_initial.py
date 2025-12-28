fabb47ef0239e4e83cc6102717389373cfb90890
Jefmans <42002519+Jefmans@users.noreply.github.com>
2025-12-29 00:29:43 +0100

    moving Town out of 0001 and into 0002

--- backend/world/migrations/0001_initial.py
+++ backend/world/migrations/0001_initial.py
@@ -96,43 +96,4 @@ class Migration(migrations.Migration):
                 ],
             },
         ),
-        migrations.CreateModel(
-            name="Town",
-            fields=[
-                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
-                ("q", models.IntegerField()),
-                ("r", models.IntegerField()),
-                ("created_at", models.DateTimeField(auto_now_add=True)),
-                (
-                    "match",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        related_name="towns",
-                        to="matches.match",
-                    ),
-                ),
-                (
-                    "province",
-                    models.OneToOneField(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        related_name="town",
-                        to="world.province",
-                    ),
-                ),
-            ],
-            options={
-                "indexes": [
-                    models.Index(
-                        fields=["match", "q", "r"],
-                        name="world_town_match_q_r_idx",
-                    ),
-                ],
-                "constraints": [
-                    models.UniqueConstraint(
-                        fields=["match", "q", "r"],
-                        name="unique_match_town_tile",
-                    )
-                ],
-            },
-        ),
     ]
